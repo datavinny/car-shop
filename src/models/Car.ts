12c229fadfad9b90a4d2fe06d8ee92243aff1757
Davi <df828316@gmail.com>
2022-09-14 15:58:27 -0300

    feat: :sparkles: Cria o model Car

---
+++ src/models/Car.ts
@@ -0,0 +1,15 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carMongooseSchema = new Schema<ICar>({
+  doorsQty: Number,
+  seatsQty: Number });
+
+class Car extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('Car', carMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default Car;
