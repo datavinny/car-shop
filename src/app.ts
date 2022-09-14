158c1126bfb8451481479aeed504db057272ca43
Davi <df828316@gmail.com>
2022-09-14 15:24:10 -0300

    feat: :sparkles: criado a rota /cars

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import carRoutes from './routes/cars';
 
 const app = express();
+app.use(carRoutes);
 
 export default app;
---
+++ src/routes/cars.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const route = Router();
+
+// route.post('/cars', (req, res) => carController.create(req, res));
+
+export default route;
