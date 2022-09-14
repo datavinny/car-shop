9b15cc98ecb95ca8367f830e5e7ef1d9d0ec2052
Davi <df828316@gmail.com>
2022-09-14 19:17:17 -0300

    feat: :sparkles: coloca o express-async-erros

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import carRoutes from './routes/Car';
+import errorHandler from './middlewares/error';
 
 const app = express();
+app.use(express.json());
 app.use(carRoutes);
+app.use(errorHandler);
 
 export default app;
